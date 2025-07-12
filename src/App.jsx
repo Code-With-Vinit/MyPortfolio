0e376ca0a46690639a24d72f2d198669a16aa030
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-07-13 00:54:09 +0530

    Set up folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React  from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Education from "./components/Education/Education";
+import Experience from "./components/Experience/Experience";
+import Skills from "./components/Skills/Skills";
+import Footer from "./components/Footer/Footer";
+import Work from "./components/Work/Work";
 
-// import './App.css'
+
 
 function App() {
 
